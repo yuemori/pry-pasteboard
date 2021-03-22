24c78d2dc14fa0e89e9e4b3bdaa1c144542fed3c
yuemori <moonoverwalker@gmail.com>
2021-03-22 14:43:34 +0900

    Initial commit

---
+++ lib/pry/clipboard.rb
@@ -0,0 +1,8 @@
+require "pry/clipboard/version"
+
+module Pry
+  module Clipboard
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/pry/clipboard/version.rb
@@ -0,0 +1,5 @@
+module Pry
+  module Clipboard
+    VERSION = "0.1.0"
+  end
+end
